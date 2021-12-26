e51391b14bacbf5c8007b796d520511fe638354e
Manotzy <46571093+Manotzy@users.noreply.github.com>
2021-12-26 02:01:28 -0300

    Add files via upload

---
+++ assets/js/anim.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    var tl = gsap.timeline();
+
+tl
+.from(".logo",{
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    delay: 0.5,
+    ease: "power3.out"
+})
+
+.from(".navbar",{
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    delay: 0.5,
+    ease: "power3.out"
+},"-=0.8")
+
+.from(".hero-item",{
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    delay: 0.5,
+    ease: "power3.out"
+},"-=0.8")
+
+.from(".hero-img",{
+    duration: 1,
+    x: 10,
+    opacity: 0,
+    delay: 0.5,
+    ease: "power3.out"
+},"-=0.8")
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#acessorios div",{
+    duration: 2,
+    y: 300,
+    opacity: 0,
+    stagger: {each: 0.2},
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: "#acessorios",
+    }
+});
+
+gsap.from("#doacao div",{
+    duration: 2,
+    y: 300,
+    opacity: 0,
+    stagger: {each: 0.2},
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: "#doacao",    
+    }
+})
+
+
+})
+
